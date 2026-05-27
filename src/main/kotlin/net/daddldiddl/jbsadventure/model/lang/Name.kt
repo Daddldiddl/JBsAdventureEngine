04a6352d8b0dec20e02fd5118427ee4da0e0ea0e
Jochen Brinkmann <41681022+Daddldiddl@users.noreply.github.com>
2026-05-27 14:23:06 +0200

    Refactor article retrieval in Name class

--- src/main/kotlin/net/daddldiddl/jbsadventure/model/lang/Name.kt
+++ src/main/kotlin/net/daddldiddl/jbsadventure/model/lang/Name.kt
@@ -2,6 +2,7 @@ package net.daddldiddl.jbsadventure.model.lang
 
 import kotlinx.serialization.Serializable
 import net.daddldiddl.jbsadventure.LANG
+import kotlin.plus
 
 @Serializable
 class Name(
@@ -13,22 +14,18 @@ class Name(
     private val regexVocalStart by lazy { "^[aeouiAEOUI]".toRegex() }
 
     fun getIndefiniteName(): String {
-        val englishException = (LANG.languageKey == "en" && name.matches(regex = regexVocalStart))
+        val englishException = (LANG.languageKey == Keys.languageKeyEn && name.matches(regex = regexVocalStart))
         val article: String = if (!isPlural){
             if(englishException) "an" else LANG.getArticle(definite=false, genderKey = genderKey)
         }
         else {
-            return "${LANG.getArticle(true, genderKey = genderKey)} $name".trim()
+            return "${LANG.getArticle(definite=false, plural=isPlural, genderKey = genderKey)} $name".trim()
         }
         return "$article $name".trim()
     }
 
     fun getDefiniteName(): String {
-        return "${LANG.getArticle(true, genderKey = genderKey)} $name".trim()
-    }
-
-    fun getIndefinitePluralName() {
-
+        return "${LANG.getArticle(definite=true, plural=isPlural, genderKey = genderKey)} $name".trim()
     }
 
     override fun toString(): String {
